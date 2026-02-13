63dea83603e39e99b1b810169547b9d85f5ff602
hades <ibuoyeemmanuel8@gmail.com>
2026-02-13 22:55:28 +0100

    modify file

--- 03_variables-and-constants/05_scope-and-shadowing/main.go
+++ 03_variables-and-constants/05_scope-and-shadowing/main.go
@@ -3,13 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	x := 10
+	// variables are only visible untill the end of the block {} they're  declared in
+	x := 10 // Scope: main function
 	fmt.Println(x)
 
+	// innner blocks can see outer blocks. outer blocks can't see inner blocks
 	if true {
-		x := 5
-		fmt.Println(x)
+		x := 5 // Scope: this if-block only. This shadows the outer x
+		fmt.Println(x) // 5
 	}
 
-	fmt.Println(x)
+	fmt.Println(x) // 10
 }
